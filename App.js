685606e267003da946dd0a2fe68bc1c9f2269dae
drnez <shayannezami@yahoo.co.uk>
2024-06-20 14:05:56 +0100

    working on navigation between screens

--- App.js
+++ App.js
@@ -1,17 +1,64 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { Dropdown } from 'react-native-element-dropdown';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Team Selection"
+                    component={TeamSelectScreen}
+                />
+                <Stack.Screen
+                    name="Home"
+                    component={HomeScreen}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+      );
+}
+
+const HomeScreen = ({navigation}) => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.headerText}>Home Screen</Text>
+            <Button
+                style={styles.navButton}
+                containerStyle={styles.buttonContainer}
+                title="Select Team"
+                onPress = { () =>
+                    navigation.navigate("Team Selection")
+                }
+            />
+
+        </View>
+    );
+}
+
+const TeamSelectScreen = ({navigation}) => {
     return (
         <View style={styles.container}>
             <Text style={styles.headerText}>Select Team</Text>
             <ScrollingTeamSelect Data={TeamNames} />
-            <StatusBar style="auto" />
+            <Button
+                style={styles.navButton}
+                containerStyle={styles.buttonContainer}
+                title="Home"
+                onPress = { () =>
+                    navigation.navigate("Home")
+                }
+            />
+            <StatusBar barStyle="auto" />
         </View>
-      );
+    );
 }
+
 
 const ScrollingTeamSelect = ({Data}) => {
     const [selectedValue, setSelectedValue] = useState(null);
@@ -36,6 +83,14 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    buttonContainer: {
+        flex: 1,
+        flexDirection: 'column',
+        position: 'absolute',
+        bottom: 0,
+        width: 80,
+        backgroundColor: '#fff',
+    },
     item: {
         padding: 10,
         fontSize: 18,
@@ -46,7 +101,13 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         fontSize: 35,
         position: 'absolute',
-        top: 65,
+        top: 10,
+        left: 15,
+    },
+    navButton: {
+        fontSize: 63,
+        position: 'absolute',
+        bottom: 0,
         left: 15,
     },
     dropdown: {
